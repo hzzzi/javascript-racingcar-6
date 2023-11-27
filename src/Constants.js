bcdd50c820b9c39dce5bab96576a7afeb69b1145
hzzzi <hji2004@naver.com>
2023-11-27 22:16:02 +0900

    feat(Constants): 입출력 메세지 상수 설정

---
+++ src/Constants.js
@@ -0,0 +1,14 @@
+const GAME_MSG = {
+  INPUT: {
+    NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    ROUND_COUNT: '시도할 횟수는 몇 회인가요?',
+  },
+  OUTPUT: {
+    ROUND_RESULT: '실행 결과',
+    WINNER: '최종 우승자 : ',
+  },
+};
+
+const NEW_LINE = '\n';
+
+export { GAME_MSG, NEW_LINE };
