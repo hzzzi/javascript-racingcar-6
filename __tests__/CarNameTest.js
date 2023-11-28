2ee48f9c3f73d935a3776d9bec9d6c4bc6b1e99c
hzzzi <hji2004@naver.com>
2023-11-28 22:28:09 +0900

    test(CarNameTest): 자동차 이름 입력값 예외 테스트 구현

---
+++ __tests__/CarNameTest.js
@@ -0,0 +1,32 @@
+import { ERROR_MSG } from '../src/Constants';
+import carNameValidator from '../src/validator/carName';
+
+describe('자동차 이름 테스트', () => {
+  test('입력값이 없으면 예외 발생', () => {
+    expect(() => {
+      const input = '';
+      carNameValidator.validateCarName(input);
+    }).toThrow(ERROR_MSG.NOTHING);
+  });
+
+  test('차를 한대만 입력하면 예외 발생', () => {
+    expect(() => {
+      const input = ['pobi'];
+      carNameValidator.validateCarName(input);
+    }).toThrow(ERROR_MSG.AT_LEAST_TWO_CARS);
+  });
+
+  test('차 이름이 5글자 초과하면 예외 발생', () => {
+    expect(() => {
+      const input = ['pobi', 'javajigi'];
+      carNameValidator.validateCarName(input);
+    }).toThrow(ERROR_MSG.MORE_THAN_FIVE_LETTERS);
+  });
+
+  test('차 이름을 중복해서 입력하면 예외 발생', () => {
+    expect(() => {
+      const input = ['pobi', 'pobi'];
+      carNameValidator.validateCarName(input);
+    }).toThrow(ERROR_MSG.DUPLICATED);
+  });
+});
