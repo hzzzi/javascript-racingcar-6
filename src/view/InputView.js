e0619683c0a415914eeab0e57b593d3b32b0eabe
hzzzi <hji2004@naver.com>
2023-11-28 15:51:35 +0900

    feat(InputView): 차 이름 입력값 쉼표로 구분하기 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -4,8 +4,11 @@ import { GAME_MSG, NEW_LINE } from '../Constants';
 const InputView = {
   async readCarName() {
     const input = await Console.readLineAsync(GAME_MSG.INPUT.NAME + NEW_LINE);
-    return input;
+    const carNames = input.split(',');
+
+    return carNames;
   },
+
   async readRoundCount() {
     const input = await Console.readLineAsync(
       GAME_MSG.INPUT.ROUND_COUNT + NEW_LINE,
